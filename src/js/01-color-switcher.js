5f18d2b007aa944460cead9910f50ec67dffd88d
MakrFrost <frostmark@ukr.net>
2022-09-07 11:27:57 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,16 +1,16 @@
 const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
+const body = document.getElementsByTagName('body')[0];
 
-startBtn.addEventListener('click', () => {
-  console.log('start pressed');
-});
-
+startBtn.addEventListener('click', bodyColor);
 stopBtn.addEventListener('click', () => {
   console.log('stop pressed');
 });
 
+function bodyColor() {
+  body.style.cssText = `background-color :${getRandomHexColor()}`;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-
-//  body.style.cssText = `width: ${size}px; height: ${size}px; background-color:${getRandomHexColor()}`;
