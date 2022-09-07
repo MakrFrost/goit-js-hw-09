c1174f827af5fafd550312fc43bdd41775b362c8
MakrFrost <101950285+MakrFrost@users.noreply.github.com>
2022-09-07 08:22:42 +0000

    Deploying to gh-pages from @ MakrFrost/goit-js-hw-09@01161c036200f0f84cf2bc540998a4671d926cd9 🚀

---
+++ 01-color-switcher.6e61c90d.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){console.log("start pressed")})),e.addEventListener("click",(function(){console.log("stop pressed")}))}();
+//# sourceMappingURL=01-color-switcher.6e61c90d.js.map
