01161c036200f0f84cf2bc540998a4671d926cd9
MakrFrost <frostmark@ukr.net>
2022-09-07 11:22:06 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,16 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+startBtn.addEventListener('click', () => {
+  console.log('start pressed');
+});
+
+stopBtn.addEventListener('click', () => {
+  console.log('stop pressed');
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+//  body.style.cssText = `width: ${size}px; height: ${size}px; background-color:${getRandomHexColor()}`;
