554a0e221ca15cff9aedb97b950142108963bdc6
MakrFrost <frostmark@ukr.net>
2022-09-08 21:26:32 +0300

    Update 3 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', newPromise);
+
+function newPromise(event) {
+  event.preventDefault();
+
+  let delay = Number(formEl.delay.value);
+  let amount = Number(formEl.amount.value);
+  let step = Number(formEl.step.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+      .then(result => Notiflix.Notify.success(result))
+      .catch(error => Notiflix.Notify.failure(error));
+
+    delay += step;
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    let shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(
+          `Fulfilled promise ${position} in ${delay}ms. Ваш промiс було виконано!`
+        );
+      }
+      reject(
+        `Rejected promise ${position} in ${delay}ms. Ваш промiс було забраковано!`
+      );
+    }, delay);
+  });
 }
