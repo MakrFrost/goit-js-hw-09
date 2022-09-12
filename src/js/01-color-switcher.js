c0f698f9564707371fccefdc679bbe1ec0c44177
MakrFrost <frostmark@ukr.net>
2022-09-12 11:22:28 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,6 +3,72 @@ const stopBtn = document.querySelector('button[data-stop]');
 const body = document.getElementsByTagName('body')[0];
 let timerId = null;
 import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+Notify.init({
+  width: '280px',
+  position: 'right-top',
+  distance: '10px',
+  opacity: 1,
+  borderRadius: '5px',
+  rtl: false,
+  timeout: 3000,
+  messageMaxLength: 110,
+  backOverlay: false,
+  backOverlayColor: 'rgba(0,0,0,0.5)',
+  plainText: true,
+  showOnlyTheLastOne: false,
+  clickToClose: false,
+  pauseOnHover: true,
+  ID: 'NotiflixNotify',
+  className: 'notiflix-notify',
+  zindex: 4001,
+  fontFamily: 'Quicksand',
+  fontSize: '13px',
+  cssAnimation: true,
+  cssAnimationDuration: 400,
+  cssAnimationStyle: 'fade',
+  closeButton: false,
+  useIcon: true,
+  useFontAwesome: false,
+  fontAwesomeIconStyle: 'basic',
+  fontAwesomeIconSize: '34px',
+  success: {
+    background: '#32C682',
+    textColor: '#fff',
+    childClassName: 'notiflix-notify-success',
+    notiflixIconColor: 'rgba(0,0,0,0.2)',
+    fontAwesomeClassName: 'fas fa-check-circle',
+    fontAwesomeIconColor: 'rgba(0,0,0,0.2)',
+    backOverlayColor: 'rgba(50,198,130,0.2)',
+  },
+  failure: {
+    background: '#FF5549',
+    textColor: '#fff',
+    childClassName: 'notiflix-notify-failure',
+    notiflixIconColor: 'rgba(0,0,0,0.2)',
+    fontAwesomeClassName: 'fas fa-times-circle',
+    fontAwesomeIconColor: 'rgba(0,0,0,0.2)',
+    backOverlayColor: 'rgba(255,85,73,0.2)',
+  },
+  warning: {
+    background: '#EEBF31',
+    textColor: '#fff',
+    childClassName: 'notiflix-notify-warning',
+    notiflixIconColor: 'rgba(0,0,0,0.2)',
+    fontAwesomeClassName: 'fas fa-exclamation-circle',
+    fontAwesomeIconColor: 'rgba(0,0,0,0.2)',
+    backOverlayColor: 'rgba(238,191,49,0.2)',
+  },
+  info: {
+    background: `${getRandomHexColor()}`,
+    textColor: '#fff',
+    childClassName: 'notiflix-notify-info',
+    notiflixIconColor: 'rgba(0,0,0,0.2)',
+    fontAwesomeClassName: 'fas fa-info-circle',
+    fontAwesomeIconColor: 'rgba(0,0,0,0.2)',
+    backOverlayColor: 'rgba(38,192,211,0.2)',
+  },
+});
 
 startBtn.addEventListener('click', bodyColorOn);
 stopBtn.addEventListener('click', bodyColorOff);
@@ -10,12 +76,13 @@ stopBtn.addEventListener('click', bodyColorOff);
 function bodyColorOn() {
   startBtn.setAttribute('disabled', true);
   timerId = setInterval(getBodyColor, 1000);
+  timerId = setInterval(getRandomHexColor, 1000);
 }
 
 function getBodyColor() {
   body.style.cssText = `background-color:${getRandomHexColor()}`;
-  Notiflix.Notify.success.background = `${getRandomHexColor()}`;
-  Notiflix.Notify.success(`Цвет изменён на ${getRandomHexColor()}`);
+  // Notify.merge({ info: { background: getRandomHexColor() } });
+  Notiflix.Notify.info(`Цвет изменён на ${getRandomHexColor()}`);
 }
 
 function bodyColorOff() {
